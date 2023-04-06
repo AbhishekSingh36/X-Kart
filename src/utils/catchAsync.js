0d97d1d3938fcfe0ac13f710db398f2a3b83f5ce
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-04-06 14:25:02 +0530

    X-Kart Backend

---
+++ src/utils/catchAsync.js
@@ -0,0 +1,6 @@
+/**
+ * Return a function that catches and forwards any error a function throws to the next middleware 
+ * 
+ * @param {Function} fn - input function that catchAsync wraps around
+ */
+
