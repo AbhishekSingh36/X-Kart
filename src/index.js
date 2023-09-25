8bc945a0c06cdcd9100c28e61896cb9f0471949b
Abhishek Singh <abhisheksingh.nyc1@gmail.com>
2023-09-26 00:33:33 +0530

    Update

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ const config = require("./config/config");
 let server;
 
 // TODO: CRIO_TASK_MODULE_UNDERSTANDING_BASICS - Create Mongo connection and get the express app to listen on config.port
+mongoose
+    .connect(config.mongoose.url, config.mongoose.options)
+    .then(() => {
+        console.log("DB is connected")
+    })
+    .catch((e) => {
+        console.log("DB not connected Error:", e)
+    })
+
